eab43367039bbb02d64595b4ada02a04fed484b3
dotkalm <joelmholmberg@gmail.com>
2019-07-19 13:51:17 -0700

    logs new song

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const songController = require('../controllers/songController')
 
 router.get('/new', songController.showNew);
+router.post('/', (req,res)=>console.log(req.body));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 const songRoutes = require('./routes/songRoutes')
+
 require('./db/db')
 
+app.use(express.json());
+app.use(express.urlencoded())
 app.use('/rikkiDontLooseThatNumber', songRoutes);
 
 app.listen(3000, () => {
